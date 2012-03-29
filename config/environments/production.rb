bdd92953beae0a0d8628a296ce45f29af3719c5a
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-03-29 17:35:39 +0530

    domain changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Sweetly::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 465,
-    domain: "sweetly.us",
+    port: 587,
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
