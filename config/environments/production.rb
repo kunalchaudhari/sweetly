2fa6005e314cfc502182a0b0e471f17b73333174
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-03-29 17:30:26 +0530

    port changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Sweetly::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 25,
+    port: 465,
     domain: "sweetly.us",
     authentication: "plain",
     enable_starttls_auto: true,
