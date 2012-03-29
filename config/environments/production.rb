cec414028d296f4d88acc7f848ccb758ca9efee3
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-03-29 23:04:24 +0530

    changed authentication to login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Sweetly::Application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: "plain",
+    authentication: "login",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
