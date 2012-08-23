9709942d5146ac4382ee094f6d1e3fd7dd87138e
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-08-23 11:16:45 +0530

    nginx conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 
 load "config/recipes/base"
-load "config/recipes/apache2"
+load "config/recipes/nginx"
 load "config/recipes/mysql"
 load "config/recipes/unicorn"
 load "config/recipes/production"
